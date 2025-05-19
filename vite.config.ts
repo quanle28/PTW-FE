569948071938d85054cbfe81bd00c728cf2114e4
quanle28 <anhquanle8204@gmail.com>
2025-05-19 17:57:46 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    allowedHosts: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      },
+    }
+  }
 })
